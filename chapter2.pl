b1318b0b77acb58fbd84a6806eb9a8c2ae6d813c
edaalii <da.li@ericsson.com>
2014-09-17 14:53:02 +0800

    practice

---
+++ chapter2.pl
@@ -0,0 +1,10 @@
+#!/bin/perl
+#$r = 3;
+$pi = 3.14;
+print "Please enter the radius of the circle:";
+chomp($r = <STDIN>);
+print "The girth of the radius($r) is ".2*$pi*$r."\n";
+
+
+
+
--- test.pl
+++ test.pl
@@ -10,16 +10,43 @@ else{
 
 	
 sub list_from_fred_to_barney {
-if($fred < $barney) {
-	#Count upwards from $fred to $barney
-	$fred .. $banrey
-} else {
-	#Count downwards from $fred to $barney
-	reverse $barney .. $fred;
-}
+	if($fred < $barney) {
+		#Count upwards from $fred to $barney
+		$fred .. $banrey
+	} else {
+		#Count downwards from $fred to $barney
+		reverse $barney .. $fred;
+	}
 }
 $fred = 11;
 $barney = 6;
 @c = &list_from_fred_to_barney;
 
 print @c;
+
+#while(<>){
+#    chomp;
+#    print join("\n",(split /:/)[0,2,1,5]),"\n";
+#}
+#@lines= `cat ~/gitCode/learnPython/user.xml`;
+#foreach(@lines){
+#	s/\w<([^>]+)/\U$1/g;
+#	print;
+#}
+
+print "barney";
+print 'barney';
+print "hello world\n";
+print "the last character of this string is a quote mark:\"";
+print "coke\tsprite";
+
+print "fred" x 3;
+print 5 x 4;
+print "\n\n";
+
+$madonna = <STDIN>;
+if( defined($madonna) ){
+print "The input was $madonna";
+}else{
+print "No input available!\n";
+}
