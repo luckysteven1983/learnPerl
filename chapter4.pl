9ce5e0f7afab559cd8c7cea84a2f38ba2a0c4743
edaalii <da.li@ericsson.com>
2014-09-20 14:04:55 +0800

    Finish chapter4 practice

--- chapter4.pl
+++ chapter4.pl
@@ -71,3 +71,27 @@ print"Enter some numbers on separate lines:\n";
 my $user_total=&total(<STDIN>);
 print"The total of those numbers is $user_total .\n";
 
+sub average{
+    foreach $n(0..$#_){
+        $sum += $_[$n];
+	}
+    $average = $sum/($#_+1);
+    }
+sub above_average{
+    @num = @_;
+    @aba = (); #aba means above average
+    $av = &average(@num);
+    foreach $n(0..$#_){
+        if( $_[$n] > $av ){
+	    push (@aba,$_[$n]);
+	    }
+	}
+    @aba;
+    }
+my @fred = &above_average(1..10);
+print "\@fred is @fred\n";
+print "(Should be 6 7 8 9 10)\n";
+
+my @barney = &above_average(100,1..10);
+print "\@barney is @barney\n";
+print "(Should be just 100)\n";
