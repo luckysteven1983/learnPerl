a1cc3b6ff9f41e7f6667f2cc65ff1c3740530a9b
edaalii <da.li@ericsson.com>
2014-09-23 14:40:50 +0800

    Finish chapter8 practice

--- chapter8.pl
+++ chapter8.pl
@@ -1,1 +1,14 @@
 #!/bin/perl -w
+while(<>){
+    chomp;
+    #if(/(\w*a)\s+/){
+    #if(/a\b/){
+    #if(/(\w*a).{5}/){
+    if(/(.*\s+)$/){
+	my $temp = $1;
+	print "$temp\$\n";
+	print "Matched: |$`<$&>$'|\n";
+    }else{
+        print "no match: |$_|\n";
+    }
+}
