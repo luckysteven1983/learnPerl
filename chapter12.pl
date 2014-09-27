4f494da789593333daa7ec58f382fb7baa5ae176
edaalii <da.li@ericsson.com>
2014-09-27 15:52:10 +0800

    Finish chapter12 practice

--- chapter12.pl
+++ chapter12.pl
@@ -1,2 +1,33 @@
 #!/bin/perl -w
-chdir "etc" or die "Cannot change dir etc:$!\n";
+#chdir "etc" or die "Cannot change dir etc:$!\n";
+#foreach $arg(@ARGV){
+#    print "one arg is $arg\n";
+#    }
+#my @files = "*.txt*; #This writing will be report error
+my @files = glob "*.txt";
+foreach $file(@files){
+    print "Files is $file\n";
+    }
+my @all_files_including = glob ".* *";
+print "@all_files_including\n";
+
+my $dir_to_process = "/etc";
+#opendir DH,$dir_to_process or die "Cannot open $dir_to_process: $!";
+#foreach $file(readdir DH){
+#    print "one file in $dir_to_process is $file\n";
+#    }
+#closedir DH;
+
+foreach my $file(qw/slate bedrock lava/){
+    unlink $file or warn "Delete failed on $file: $!\n";
+    }
+
+$folder = <STDIN>;
+if($folder){
+    chdir $folder or die "No such folder: $!\n";
+    }else{
+    $folder = "~";
+    chdir $folder;
+    }
+@files_list = glob "*";
+print "@files_list";
